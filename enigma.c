f63295558ae9bd776b630b23ec23759e62cf908c
amirmozafari <kavian.amirmozafari@student.sharif.edu>
2020-08-05 18:49:07 +0430

    Add rotating the rotors

--- enigma.c
+++ enigma.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-void fill_rotor(char const *);
+void fill_rotors(char const *);
 char* encode(char*);
 int find_char(char);
 int lookup(int *, int);
+void rotate_rotors(int);
 
 int r1[26];
 int r2[26];
 int r3[26];
 
 int main(){
-	fill_rotor("codebook.txt");
-	char input[20];
+	fill_rotors("codebook.txt");
+	char input[256];
 	printf("Enter your inputs: ");
 	scanf("%s", input);
 
@@ -22,7 +23,7 @@ int main(){
 	free(output);
 }
 
-void fill_rotor(char const *path){
+void fill_rotors(char const *path){
 	FILE *fptr;
 	fptr = fopen(path, "r");
 
@@ -37,7 +38,7 @@ void fill_rotor(char const *path){
 	}
 	fclose(fptr);
 
-	/*	
+	///*	
 	for(int i=0; i<26; i++) {
 		printf("%d ", r1[i]);
 	}
@@ -50,7 +51,7 @@ void fill_rotor(char const *path){
 		printf("%d ", r3[i]);
 	}
 	printf("\n");
-	*/
+	//*/
 
 }
 
@@ -58,40 +59,20 @@ char* encode(char *input) {
 	//char output[strlen(input)];
 	char *output = (char*)malloc(strlen(input));
 	for(size_t i=0; i<strlen(input); i++){
-	
 		int x = find_char(input[i]);
 		int r1_output = r1[x];
 		int r2_output = r2[r1_output];
 		int r3_output = r3[r2_output];
 		int reflector = 25 - r3_output;
 
-		/*
-		printf("r1 output: %d\n", r1_output); 
-		printf("r2 output: %d\n", r2_output);
-		printf("r3 output: %d\n", r3_output);
-		printf("rotater output: %d\n", reflector);
-		*/
-		
 		int r3_input = lookup(r3, reflector);
 		int r2_input = lookup(r2, r3_input);
 		int r1_input = lookup(r1, r2_input);
 
-		/*
-		int r1_input = find_char(input);
-		int r2_input = lookup(r1, r1_input);
-		int r3_input = lookup(r2, r2_input);
-		int rotater_input = lookup(r3, r3_input);
-		int rotater_output = 25 - rotater_input;
-
-		*/	
 		char const *alpha = "abcdefghijklmnopqrstuvwxyz"; 
-		/*
-		printf("r3 input: %d\n", r3_input);
-		printf("r2 input: %d\n", r2_input);
-		printf("r1 input: %d\n", r1_input);
-		printf("Output: %c\n", alpha[r1_input]);
-		*/
 		output[i] = alpha[r1_input];
+
+		rotate_rotors(i+1);
 	}
 	return output;
 }
@@ -114,3 +95,33 @@ int lookup(int *r, int input) {
 	return -1;
 }
 
+void rotate_rotors(int count){
+	if(count%26 == 0){
+		int last = r2[25];
+		for(int i=24; i>=0; i--)
+			r2[i+1] = r2[i];
+		r2[0] = last;
+		
+		for(int i=0; i<26; i++) {
+			printf("%d ", r2[i]);
+		}
+		printf("\n");
+	} else if(count%676 == 0) {
+		int last = r3[25];
+		for(int i=24; i>=0; i--)
+			r3[i+1] = r3[i];
+		r3[0] = last;
+	} else{
+		int last = r1[25];
+		for(int i=24; i>=0; i--)
+			r1[i+1] = r1[i];
+		r1[0] = last;
+		/*
+		for(int i=0; i<26; i++) {
+			printf("%d ", r1[i]);
+		}
+		printf("\n");
+		*/
+		
+	}
+}
